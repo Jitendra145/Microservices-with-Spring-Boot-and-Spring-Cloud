10e20eeca6c5f6794306c21f351a0354c267e6f9
Jitendra <jitenverma145@gmail.com>
2019-07-16 22:58:39 +0530

    "Microservices-with-Spring-Boot-and-Spring-Cloud"

---
+++ currency-conversion-service/src/main/java/com/jitendra/microservice/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jitendra.microservice.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
